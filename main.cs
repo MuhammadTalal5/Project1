38bfc4ed375f8b1ca70ae7ba87bd32e26ca51ac5
MuhammadTalal5 <108627029+MuhammadTalal5@users.noreply.github.com>
2022-07-04 07:54:19 +0500

    updated

--- main.cs
+++ main.cs
@@ -6,26 +6,26 @@ public class SalarySystem
         private int baseSalary;
         private int grossSalary;
 
-        public SalarySlip(int baseSalary){
-            this.baseSalary = baseSalary;
+        public SalarySlip(int bSalary){
+            baseSalary = bSalary;
         }
 
         public void calculateSalary(string designation){
             if (designation == "engineer")
             {
-                this.grossSalary = this.baseSalary + 100 + 500;
+                grossSalary = baseSalary + 100 + 500;
             } else if (designation == "analyst"){
-                this.grossSalary = this.baseSalary + 150 + 800;
+                grossSalary = baseSalary + 150 + 800;
             }else if(designation == "manager"){
-                this.grossSalary = this.baseSalary + 250 + 1000;
+                grossSalary = baseSalary + 250 + 1000;
             }else{
-                this.grossSalary = 0;
+                grossSalary = 0;
             }
         }
 
         public void displaySalary(){
-            if(this.grossSalary != 0){
-                Console.WriteLine(this.grossSalary);
+            if(grossSalary != 0){
+                Console.WriteLine(grossSalary);
             }else{
                 Console.WriteLine("Please enter valid designation!");
             }
